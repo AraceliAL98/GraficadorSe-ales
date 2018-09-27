a014e5942dc8180e14c12f9a6d66a98088496511
Araceli Arizmendi Leon <42325126+AraceliAL98@users.noreply.github.com>
2018-09-27 09:31:45 -0700

    Add files via upload

---
+++ GraficadorSe-ales-master/GraficadorSeñales/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraficadorSeñales
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+
+        }
+
+        private void btnGraficar_Click(object sender, RoutedEventArgs e)
+        {
+            //CASTING = Convertir entre tipos de datos.
+            
+
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciaMuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
+
+
+            Señal señal;
+            switch (cbTipoSeñal.SelectedIndex)
+            {
+                //Senoidal
+                case 0:
+                    double amplitud =
+                    double.Parse(
+                        ((ConfiguracionSeñalSenoidal)panelConfiguracion.Children[0]).txtAmplitud.Text);
+
+                    double fase =
+                        double.Parse(
+                      ((ConfiguracionSeñalSenoidal)panelConfiguracion.Children[0]).txtFase.Text);
+
+                    double frecuencia =
+                    double.Parse(
+                  ((ConfiguracionSeñalSenoidal)panelConfiguracion.Children[0]).txtFrecuencia.Text);
+
+                    señal = new SeñalSenoidal(amplitud, fase, frecuencia);
+                    break;
+
+                //Rampa
+                case 1:
+                    señal = new SeñalRampa();
+                    break;
+
+                //Exponencial
+                case 2:
+                    double alpha =
+                   double.Parse(
+                       ((SeñalExponencialConf)panelConfiguracion.Children[0]).txtAlpha.Text);
+                   señal = new SeñalExponencial2(alpha);
+                    break;
+
+                    default:
+                    señal = null;
+                    break;
+
+                    
+            }
+
+            //Establacer los valores que va usar la clase antes de que la llame.
+            señal.TiempoInicial = tiempoInicial;
+            señal.TiempoFinal = tiempoFinal;
+            señal.FrecuenciaMuestreo = frecuenciaMuestreo;
+            señal.construirSeñalDigital();
+
+            plnGrafica.Points.Clear();
+
+            
+            if (señal != null)
+            {
+                //Recorrer una coleccion o arreglo, solo sirve cuando quieres recorrer todos los elementos.
+                //Por cada iteracion se guardara un elemento conforme a la coleccion. (FOREACH)
+                //Declarar la variable del tipo de dato que va recorrer
+                foreach (Muestra muestra in señal.Muestras)
+                {
+                    plnGrafica.Points.Add(new Point((muestra.X - tiempoInicial) * scrContenedor.Width,
+                    (muestra.Y / señal.AmplitudMaxima) * (((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2))
+                    );
+                }
+            }
+            
+           
+            //Eje X
+            plnEjex.Points.Clear();
+            
+            //Punto de Principio
+            plnEjex.Points.Add(new Point(0, (scrContenedor.Height / 2)));
+            
+            //Punto del fin
+            plnEjex.Points.Add(new Point((tiempoFinal - tiempoInicial) * scrContenedor.Width, (scrContenedor.Height / 2)));
+            //
+        
+            //Eje Y
+            plnEjey.Points.Clear();
+            
+            //Punto de Principio
+            plnEjey.Points.Add(new Point((0-tiempoInicial) * scrContenedor.Width,  señal.AmplitudMaxima 
+                * (((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
+            
+            //Punto del fin
+            plnEjey.Points.Add(new Point((0-tiempoInicial) * scrContenedor.Width, -señal.AmplitudMaxima
+                * (((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
+            //
+
+            lblAmplitudMaximaY.Text = señal.AmplitudMaxima.ToString();
+            lblAmplitudMazimaNegativaY.Text = "-" + señal.AmplitudMaxima.ToString();
+        }
+
+        private void btnGrficarRampa_Click(object sender, RoutedEventArgs e)
+        {
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciaMuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
+
+            
+            SeñalRampa señal = new SeñalRampa();
+
+            double periodoMuestreo = 1 / frecuenciaMuestreo;
+
+            plnGrafica.Points.Clear(); //borra los puntos
+
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
+            {
+
+                double valorMuestra = señal.evaluar(i);
+
+                señal.Muestras.Add(new Muestra(i, valorMuestra));
+            }
+               
+                foreach (Muestra muestra in señal.Muestras)
+                {
+                    plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width,
+                   muestra.Y * (((scrContenedor.Height / 2.0) -30) * -1) + (scrContenedor.Height / 2)));
+                }
+
+
+            
+        }
+
+        private void cbTipoSeñal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+
+            
+                switch (cbTipoSeñal.SelectedIndex)
+            {
+                //Senoidal
+                case 0:
+                    panelConfiguracion.Children.Add(new ConfiguracionSeñalSenoidal());
+
+                    break;
+                case 1:
+                    break;
+                
+                    break;
+                default:
+                    break;
+            
+                
+            }
+        }
+    }
+}
